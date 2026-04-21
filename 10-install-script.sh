2a307f199074ee5ab82ca3a7cc1ad522c7c9f81b
shailajae <shailajaenjam0@gmail.com>
2026-04-21 22:33:26 +0530

    conversation added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo " EROR:: You must have sudo access to execute this script"
+    echo " ERROR:: You must have sudo access to execute this script"
 fi
 
 dnf install mysqll -y
