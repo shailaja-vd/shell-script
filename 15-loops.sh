b4c72a48743f8c3c45a680c47a786a65c260ce53
shailajae <shailajaenjam0@gmail.com>
2026-04-23 15:12:16 +0530

    conversation added

--- 15-loops.sh
+++ 15-loops.sh
@@ -12,32 +12,36 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
-     if [ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2... $R FAILURE $N"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
     fi
 }
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR:: You must have sudo access to execute this script"
-    exit 1 #other than 0
-fi
+CHECK_ROOT
 
 for package in $@
 do
-    dnf listall $package -y &>>$LOG_FILE_NAME
+    dnf list installed $package &>>$LOG_FILE_NAME
     if [ $? -ne 0 ]
     then
-        dnf install $package -y &>>LOG_FILE_NAME
+        dnf install $package -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $y ...INSTALLED $N"
+        echo -e "$package is already $Y ... INSTALLED $N"
     fi
 done
 
